ba70a93c4b0b267c6b887e77045dd27ad417bbe3
ratiasrozicki <49293440+ratiasrozicki@users.noreply.github.com>
2020-04-01 22:36:35 -0300

    Hola Mundo Recursante

--- Hola Mundo/Hola Mundo/Program.vb
+++ Hola Mundo/Hola Mundo/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Hello World Recursante!")
     End Sub
 End Module
